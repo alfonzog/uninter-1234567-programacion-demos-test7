ac5563627db8533226f9d16f723c06b6743b74bd
José Armando Alfonzo García <alfonzog@hotmail.com>
2026-08-19 08:08:34 -0600

    Suma de 2 números

--- test7/test7.cpp
+++ test7/test7.cpp
@@ -12,6 +12,19 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+  int a = 0;
+  int b = 0;
+  int suma = 0;
+
+  printf("Digite un numero: ");
+  scanf_s("%d", &a);
+
+  printf("Digite otro numero: ");
+  scanf_s("%d", &b);
+
+  suma = a + b;
+
+  std::cout << "La suma de " << a << " y " << b << " es: " << suma << std::endl;
+
 }
 
